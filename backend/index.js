7c0b4dd6ee427edf21263af5c1a232e361c089fb
@RupertoTacuche <murcio2490@gmail.com>
2024-02-27 13:48:09 -0600

    primer_commit

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import conectarDB from "./config/db.js"
+import dotenv from "dotenv"
+
+const app = express()
+
+dotenv.config()
+
+conectarDB();
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Servidor en el puerto ${PORT}`)
+})
