992a051c59d645aa6144139535c25852acca59aa
@RupertoTacuche <murcio2490@gmail.com>
2024-02-27 18:59:16 -0600

    video_125

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -2,7 +2,19 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res)  => {
-    res.send("desde API/USUARIOS");
+    res.json("Desde API/USUARIOS");
+})
+
+router.post("/confirmar", (req, res)  => {
+    res.json({msg: "Confirmando  usuario"});
+})
+
+router.put("/", (req, res) => {
+    res.json("DESDE - PUT - API/USUARIOS")
+})
+
+router.delete("/", (req, res) => {
+    res.json("DESDE - DELETE - API/USUARIOS")
 })
 
 export default router;
